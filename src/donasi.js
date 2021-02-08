bc92e1799fbe1b24d2664c46c4adf28b26fcde78
ahmadzakibarimartin <43287018+ahmadzakibarimartin@users.noreply.github.com>
2021-02-08 11:02:11 +0700

    Update donasi.js

--- src/donasi.js
+++ src/donasi.js
@@ -4,10 +4,9 @@ const donasi = () => {
  فَاتَّقُوا النَّارَ وَلَوْ بِشِقِّ تَمْرَةٍ فَمَنْ لَمْ يَجِدْ فَبِكَلِمَةٍ طَيِّبَةٍ
 _“jauhilah api neraka, walau hanya dengan bersedekah setengah biji kurma atau satu biji kurma(sedikit). Jika kamu tidak punya, maka bisa dengan kalimah thayyibah” [HR. Bukhari 6539, Muslim 1016]_
 
-*Pulsa :* _085781406716_
-*Dana :* _085781406716_
-*OVO :* _085781406716_
-*Gopay* _085781406716_`
+*Pulsa :* _083873394995_
+*Saweria*: https://saweria.co/overlays/alert?streamKey=771db43463e343b4338ab82173dbbccf&template=baru+saja+memberikan&backgroundColor=%23faae2b&highlightColor=%23744fc9&color=%23333333&fontWeight=500&notificationTimeout=5000
+
 }
 
 exports.donasi = donasi
